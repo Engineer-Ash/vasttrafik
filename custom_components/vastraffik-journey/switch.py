f61da466ece1352e7989a6dc73345b4f0acfa9d3
Engineer-Ash <ava.sanders@volvocars.com>
2025-06-05 15:52:55 +0200

    Refactor VasttrafikPauseSwitch to use sensor objects directly and simplify pause handling

--- custom_components/vastraffik-journey/switch.py
+++ custom_components/vastraffik-journey/switch.py
@@ -1,49 +1,31 @@
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.helpers.entity_component import async_update_entity
+from .sensor import VasttrafikJourneySensor
 
 async def async_setup_entry(hass, entry, async_add_entities):
-    # Get all journey sensor entity_ids for this config entry from entity registry
-    entity_registry = await hass.helpers.entity_registry.async_get_registry()
-    sensor_entity_ids = [entity.entity_id for entity in entity_registry.entities.values()
-                        if entity.domain == 'sensor' and entity.config_entry_id == entry.entry_id]
-    switches = [VasttrafikPauseSwitch(entity_id) for entity_id in sensor_entity_ids]
+    # Use sensors stored in hass.data by the sensor platform
+    sensors = hass.data.get("vastraffik_journey_sensors", [])
+    switches = [VasttrafikPauseSwitch(sensor, hass) for sensor in sensors if isinstance(sensor, VasttrafikJourneySensor)]
     async_add_entities(switches, True)
 
 class VasttrafikPauseSwitch(SwitchEntity):
-    """Switch to pause/unpause a VasttrafikJourneySensor by entity_id."""
-    def __init__(self, sensor_entity_id):
-        self._sensor_entity_id = sensor_entity_id
-        self._attr_unique_id = f"pause_{sensor_entity_id.replace('.', '_')}"
-        self._attr_name = f"Pause {sensor_entity_id.split('.')[-1].replace('_', ' ').title()}"
+    """Switch to pause/unpause a VasttrafikJourneySensor."""
+    def __init__(self, sensor: VasttrafikJourneySensor, hass):
+        self._sensor = sensor
+        self._hass = hass
+        self._attr_unique_id = f"pause_{sensor._attr_unique_id}"
+        self._attr_name = f"Pause {sensor.name}"
         self._attr_icon = "mdi:pause-circle"
         self._attr_entity_category = "config"
-        self._hass = None
-
-    async def async_added_to_hass(self):
-        self._hass = self.hass
 
     @property
     def is_on(self):
-        # Read the paused attribute from the sensor's state
-        state = self._hass.states.get(self._sensor_entity_id)
-        if state and 'paused' in state.attributes:
-            return state.attributes['paused']
-        return False
+        return self._sensor._paused
 
     async def async_turn_on(self, **kwargs):
-        # Call the custom pause service to pause the sensor
-        await self._hass.services.async_call(
-            "vastraffik_journey", "set_pause",
-            {"entity_id": self._sensor_entity_id, "paused": True},
-            blocking=True
-        )
-        await async_update_entity(self._hass, self._sensor_entity_id)
+        self._sensor.set_paused(True)
+        await async_update_entity(self._hass, self._sensor.entity_id)
 
     async def async_turn_off(self, **kwargs):
-        # Call the custom pause service to unpause the sensor
-        await self._hass.services.async_call(
-            "vastraffik_journey", "set_pause",
-            {"entity_id": self._sensor_entity_id, "paused": False},
-            blocking=True
-        )
-        await async_update_entity(self._hass, self._sensor_entity_id)
+        self._sensor.set_paused(False)
+        await async_update_entity(self._hass, self._sensor.entity_id)
